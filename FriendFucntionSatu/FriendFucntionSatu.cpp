628d3ea14310e03e8b538362985d22ad83770b68
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-06 11:08:59 +0700

    return 0;

---
+++ FriendFucntionSatu/FriendFucntionSatu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+	string nama;
+
+public:
+	friend void setNama(mahasiswa& a, string);
+};
+
+void setNama(mahasiswa& a, string pNama)
+{
+	a.nama = pNama;
+}
+int main()
+{
+	mahasiswa joko;
+	setNama(joko, "joko Kumar");
+	return 0;
+}
+
